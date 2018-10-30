9ccc9ccb1f2b5ec30bbb790a00d1ac4cc97b02d6
Andrew-Gibson42 <42543554+Andrew-Gibson42@users.noreply.github.com>
2018-10-30 11:29:19 -0500

    Add files via upload

---
+++ Javascript1Assignment/jsVariables.js
@@ -0,0 +1,9 @@
+var array = [1, 2, 3, 4, 5, 6];
+
+document.getElementById("first").textContent = array[(Math.floor(Math.random() * 6))];
+
+document.getElementById("second").textContent = array[(Math.floor(Math.random() * 6))];
+
+document.getElementById("third").textContent = array[(Math.floor(Math.random() * 6))];
+
+document.getElementById("fourth").textContent = array[(Math.floor(Math.random() * 6))];
